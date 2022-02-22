5e76d19589efb08e93b93e3676d618cbadcb3a7d
niklas.mertsch <niklas.mertsch@stud.uni-goettingen.de>
2022-02-22 09:59:47 +0100

    Migrate requirements from requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="ot2-controller",
     packages=find_packages(),
-    install_requires=["sila2"],
+    install_requires=[
+        "opentrons==3.20.1",
+        "opentrons-shared-data==3.20.1",
+        "paramiko==2.7.2",
+        "scp==0.13.2",
+        "sila2==0.5.5",
+    ],
     include_package_data=True,
 )
