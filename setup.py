3b7acd0af3018fcd4712450caf481622e9c9c15a
Florian Bauer <florian.bauer.dev@gmail.com>
2023-06-04 18:09:55 +0200

    chore: Incremented versions and stretched dependency version range.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="ot2_controller",
-    version="0.2.0",
+    version="0.3.0",
     author="Florian Bauer",
     author_email="<florian.bauer.dev@gmail.com>",
     packages=find_packages(),
     install_requires=[
-        "paramiko==2.7.2",
-        "scp==0.13.2",
-        "sila2==0.5.5",
+        "paramiko >=2.7.2, <2.9.0",
+        "scp <=0.13.2, <0.15.0",
+        "sila2 ==0.10.4",
     ],
     include_package_data=True,
 )
